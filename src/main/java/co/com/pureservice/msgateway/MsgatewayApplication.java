0cf14169f5081baafb64bfc61e2b4cc4b767aca3
angrajales <agrajal7@eafit.edu.co>
2019-09-28 08:49:41 -0500

    Init ZUUL and Eureka

--- src/main/java/co/com/pureservice/msgateway/MsgatewayApplication.java
+++ src/main/java/co/com/pureservice/msgateway/MsgatewayApplication.java
@@ -2,8 +2,12 @@ package co.com.pureservice.msgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaServer
 public class MsgatewayApplication {
 
 	public static void main(String[] args) {
